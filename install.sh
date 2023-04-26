34a5c9cdb1592319d7a5c77a9648d6608a1a7b7a
Sebastian Hestsveen <sebashes@stud.ntnu.no>
2023-04-26 17:34:11 +0200

    uppdate scritpt

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo su
-docker run -t owasp/zap2docker-stable zap-full-scan.py -t http://172.16.10.101:3000/
+docker run -t owasp/zap2docker-stable zap-full-scan.py -t http://172.16.10.101:3001/
