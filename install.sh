44b5b626ef881c6b02282d282703b698c0e7e36a
SebastianHestsveen <70824743+SebastianHestsveen@users.noreply.github.com>
2023-05-20 11:49:09 +0200

    add forgoten char &

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo su
-sudo docker run --user root -v $(pwd):/zap/wrk:rw -t owasp/zap2docker-stable zap-baseline.py -t http://172.16.10.100:3000/ -r testresults.html
+sudo docker run --user root -v $(pwd):/zap/wrk:rw -t owasp/zap2docker-stable zap-baseline.py -t http://172.16.10.100:3000/ -r testresults.html &
